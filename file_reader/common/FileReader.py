767afd3d063035c831ad18583d734b50b86e5a18
joaquinhojman <hojmanjoaquin@gmail.com>
2023-04-23 11:09:36 -0300

    Refact on file reader

--- file_reader/common/FileReader.py
+++ file_reader/common/FileReader.py
@@ -22,38 +22,39 @@ class FileReader:
         logging.info(f'action: Handle SIGTERM | result: success')
 
     def run(self):
-        self._send_data("montreal/weather.csv")
+        self._send_data("montreal/weather.csv", send_topic=True)
         self._send_data("toronto/weather.csv")
-        self._send_data("washington/weather.csv", True)
-        self._send_data("montreal/stations.csv")
+        self._send_data("washington/weather.csv", send_eof=True)
+        self._send_data("montreal/stations.csv", send_topic=True)
         self._send_data("toronto/stations.csv")
-        self._send_data("washington/stations.csv", True)
-        self._send_data("montreal/trips.csv")
+        self._send_data("washington/stations.csv", send_eof=True)
+        self._send_data("montreal/trips.csv", send_topic=True)
         self._send_data("toronto/trips.csv")
-        self._send_data("washington/trips.csv", True)
+        self._send_data("washington/trips.csv", send_eof=True)
         self._receive_results()
         self._close_connection()
 
-    def _send_data(self, file_path, send_eof=False):
+    def _send_data(self, file_path, send_topic=False, send_eof=False):
         data_type = os.path.splitext(file_path)[0].split('/')[1]
         city_name = os.path.splitext(file_path)[0].split('/')[0]
+
+        if (send_topic): self._send_topic(data_type)
 
         self._f = open(self._bets_file, 'r')
         row_header = self._f.readline().split(',')
         eof = False
         while not eof:
             data, eof = self._get_data(row_header, data_type, city_name)
-            self._protocol.send(data)
-            ack = self._protocol.receive_ack()
-            if not ack:
-                raise OSError("Socket connection broken during send data")
+            self._send(data)
         self._f.close()
 
-        if (send_eof):
-            self._protocol.send(self._get_eof_packet(data_type))
-            ack = self._protocol.receive_ack()
-            if not ack:
-                raise OSError("Socket connection broken during send eof")
+        if (send_eof): self._send_eof(data_type)
+
+    def _send_topic(self, data_type):
+        self._send(self._get_topic_packet(data_type))
+
+    def _get_topic_packet(self, data_type):
+        return data_type + ";0"
 
     def _get_data(self, row_header: list, data_type: str, city_name: str):
         data = ""
@@ -84,8 +85,17 @@ class FileReader:
         row = row[:-1] + "}"
         return row
 
+    def _send_eof(self, data_type):
+        self._send(self._get_eof_packet(data_type))
+
     def _get_eof_packet(self, data_type):
         return data_type + ";1"
+
+    def _send(self, data):
+        self._protocol.send(data)
+        ack = self._protocol.receive_ack()
+        if not ack:
+            raise OSError("Socket connection broken during send data")
     
     def _receive_results(self):
         data = self._protocol.receive()
